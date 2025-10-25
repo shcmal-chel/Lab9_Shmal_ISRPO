2c38cfef40ef16348ac35de3e1c84700664e99a9
Ivan <shcmalivan@gmail.com>
2025-10-25 18:05:12 +0300

    Add files via upload

---
+++ Lab_9_homework/MathLibrary/MathTools.cs
@@ -0,0 +1,8 @@
+﻿namespace MathLibrary
+{
+    public class MathTools
+    {
+        public int Add(int x, int y) { return x + y; }
+        public int Multiply(int x, int y) {return x * y; }
+    }
+}
